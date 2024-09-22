a0ab7332b7e1e00ccd7b66c62eb706979397b106
Cheng Wu <cwu57cn@gmail.com>
2024-09-22 17:27:13 -0500

    Create circle bar in Skills section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+/*Create circular points for circle bar*/
+const circles = document.querySelectorAll(".circle");
+
+const pointFunction = function (element) {
+  let points = "";
+  const dots = element.getAttribute("dots");
+  const percent = element.getAttribute("percent");
+  const neonPoints = Math.floor(dots * percent);
+  const rotate = 360 / dots;
+
+  for (let i = 0; i < dots; i++) {
+    points += `<div class="point" style="--i:${i}; --rotate:${rotate}deg"></div>`;
+  }
+  element.innerHTML = points;
+
+  const allPoints = element.querySelectorAll(".point");
+  for (let i = 0; i < neonPoints; i++) {
+    allPoints[i].classList.add("neon");
+  }
+};
+
+circles.forEach(pointFunction);
